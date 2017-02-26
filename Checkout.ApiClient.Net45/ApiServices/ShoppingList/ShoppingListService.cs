d783e8a333a6e58e1e0b0871838f24381b0d0dff
Fabio Salvalai <fabio@seesharp.ch>
2017-02-26 19:05:58 +0100

    Notes on pagination.

--- Checkout.ApiClient.Net45/ApiServices/ShoppingList/ShoppingListService.cs
+++ Checkout.ApiClient.Net45/ApiServices/ShoppingList/ShoppingListService.cs
@@ -9,6 +9,16 @@ using Checkout.ApiServices.ShoppingList.Model;
 
 namespace Checkout.ApiServices.ShoppingList
 {
+    /// <summary>
+    /// The .net wrapper for the ShoppingList API.
+    /// </summary>
+    /// <remarks>
+    /// Paging was not implemented for time reasons. The approach that I would have taken however would have been different from the existing services.
+    /// I probably would have created a Paginable<T> and make it implement IEnumerable<T>. That way, the api wrapper would have abstracted the pagination for the consumer.
+    /// 
+    /// In the current implementation, the consumer needs to be aware of the offsets to provide. Using an IEnumerable would allow us to transparently query the next page when elements
+    /// can no longer be yielded from the payload that we already have retrieved.
+    /// </remarks>
     public class ShoppingListService
     {
         private readonly BodylessResponseFriendlyPayloadDeserializer<ModelErrorCollection> bodylessResponseFriendlyPayloadDeserializer = new BodylessResponseFriendlyPayloadDeserializer<ModelErrorCollection>();
