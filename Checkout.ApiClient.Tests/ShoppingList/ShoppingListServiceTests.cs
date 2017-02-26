2ad269a5596d8a5819510f466dece5ffe4f8aaa4
Fabio Salvalai <fabio@seesharp.ch>
2017-02-26 11:41:33 +0100

    write test for ordering a drink

--- Checkout.ApiClient.Tests/ShoppingList/ShoppingListServiceTests.cs
+++ Checkout.ApiClient.Tests/ShoppingList/ShoppingListServiceTests.cs
@@ -19,5 +19,18 @@ namespace Tests.ShoppingList
             orderedDrinks.Model.Should().NotBeNull();
         }
 
+        [Test]
+        public void OrderDrink()
+        {
+            string drinkName;
+            var response = this.CheckoutClient.ShoppingListService.OrderDrink();
+            response.HttpStatusCode.Should().Be(HttpStatusCode.OK);
+
+            var orderedDrinks = this.CheckoutClient.ShoppingListService.GetOrderedDrinks();
+            orderedDrinks.Should().NotBeNull();
+            orderedDrinks.HttpStatusCode.Should().Be(HttpStatusCode.OK);
+            orderedDrinks.Model.Should().NotBeNull();
+            orderedDrinks.Model.Should().Contain(order => order.Name == drinkName);
+        }
     }
 }
